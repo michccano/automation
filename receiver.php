63da1dfbc710d95ec65981fd38cf9dd66a778a0d
Fahim Foysal <ffoysal6@gmail.com>
2021-08-25 05:59:33 +0600

    fix- data pass

--- receiver.php
+++ receiver.php
@@ -1,17 +1,26 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // receive data
-    $property_data = $_POST;
+// receive data
+$property_data = $_POST;
 
-    try {
-        if (count($data) > 0) {
-            //  --- run script ---
-            $r = shell_exec("python3 script.py" . escapeshellarg(json_encode($property_data)));
-            echo $r;
-        }
-    } catch (Exception $e) {
-        print_r($e);
+// $property_data = [
+//     "post-title-0" => "post title",
+//     "property_price" => "1000",
+//     "property_size" => "1200",
+//     "size_postfix" => "sq ft",
+//     "property_id" => "1234",
+//     "year_built" => "2000"
+// ];
+
+try {
+    if (count($property_data) > 0) {
+        //  --- run script ---
+        $res = shell_exec('python3 script.py ' . escapeshellarg(json_encode($property_data)));
+        echo $res;
     }
+} catch (Exception $e) {
+    print_r($e);
+}
 } else {
-    echo "Hello Alien";
+echo "Hello Alien";
 }
