b76787b1f00c77b53217d79c56cf9d7881c78048
Fahim Foysal <ffoysal6@gmail.com>
2021-08-30 17:21:11 +0600

    update receiver

--- property_update_receiver.php
+++ property_update_receiver.php
@@ -1,1 +1,35 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $property_data = ($_POST);
+} else {
+    $property_data = [
+        "post-title-0" => "Demo Post",
+        "property_price" => "",
+        "property_size" => 749,
+        "size_postfix" => "sqm",
+        "property_id" => 9110094,
+        "year_built" => 1973,
+        "property_type" => "Residential",
+        "type_name" => "House",
+        "property_status" => "sale",
+        "property_address" => "16 Evans Road, Western Australia, 6330, Australia",
+        "property_description" => [
+            "Residential",
+            "749 sqm",
+            "Contact Kathleen Mier 08 9841 1455 for more info."
+        ],
+        "photos" => [
+            "https://s3-ap-southeast-2.amazonaws.com/photos-clientvault-com/2326/40480083__1629939683-16541-MI232-1.jpg",
+        ]
+    ];
+}
+
+ try {
+     if (count($property_data) > 0) {
+         $res = shell_exec('sudo python3 receiver_test.py ' . escapeshellarg(json_encode($property_data)));
+         shell_exec('sudo rm photo*');
+         echo $res;
+     }
+ } catch (Exception $e) {
+     print_r($e);
+ }
